db1af6d77c261aa65ff38283db7ecb25bd47a170
dongjin5478 <95131996+dongjin5478@users.noreply.github.com>
2022-02-06 11:28:07 +0530

    Update ReportController.java

--- src/main/java/com/student/api/controller/ReportController.java
+++ src/main/java/com/student/api/controller/ReportController.java
@@ -3,7 +3,8 @@ package com.student.api.controller;
 
 import java.io.IOException;
 import java.util.List;
-
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
