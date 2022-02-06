64a1fb70381895ec507935d656f535ce29d41b2a
dongjin5478 <95131996+dongjin5478@users.noreply.github.com>
2022-02-06 11:30:30 +0530

    Update ReportController.java

--- src/main/java/com/student/api/controller/ReportController.java
+++ src/main/java/com/student/api/controller/ReportController.java
@@ -9,12 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.student.api.response.ApiResponse;
 import com.student.api.request.WeightSlipRequest;
 import com.student.api.response.ReportResponse;
 import com.student.api.service.ReportServiceImpl;
