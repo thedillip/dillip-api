1d8b16b13ae0f4728cf2a69a85ad99d1e47cc1a6
dongjin5478 <dongjinmaster9@gmail.com>
2022-05-14 11:03:16 +0530

    Added ProjectConstant.java

---
+++ src/main/java/com/student/api/util/ProjectConstant.java
@@ -0,0 +1,12 @@
+package com.student.api.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProjectConstant {
+	public static final String SUCCESS_MSG = "SUCCESS";
+	public static final String ERR_MSG = "ERROR";
+	public static final String DELETED_MSG = "Resource Deleted";
+	public static final String DATA_FOUND = "Data Found";
+	public static final String DATA_NOT_FOUND = "Data not Found";
+}
